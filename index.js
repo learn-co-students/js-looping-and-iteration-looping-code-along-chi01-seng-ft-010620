3c6b4cbd704dbc65c728c55047d64633240dbaba
Zach Weber <weberzt@gmail.com>
2020-02-16 16:07:23 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(friendNames, eventName) {
+    let finishedCards = []
+    for (let i = 0; i < friendNames.length; i++) {
+        finishedCards.push(`Thank you, ${friendNames[i]}, for the wonderful ${eventName} gift!`)
+    }
+    return finishedCards
+}
+
+function countDown(countdown) {
+    while(countdown > 0) {
+        console.log(countdown)
+        countdown -= 1
+    }
+    console.log(countdown)
+}
