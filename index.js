754a6eb88263971d12f49f4e43b8fd11cdb4da4d
Lindsay Mecher <lindsaymecher@gmail.com>
2020-02-17 02:59:26 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solutions in this file
+
+
+// function writeCards(array, greeting) {
+//   for (let i = 0; i < array.length; i++) {
+//     console.log(`Thank you, ${array[i]}, for the wonderful ${greeting} gift!`)
+//   }
+//   return array;
+// };
+
+function writeCards( array, greeting) {
+  let cardsMessage = [];
+  for ( let i = 0; i < array.length; i++ ) {
+    cardsMessage.push( `Thank you, ${array[i]}, for the wonderful ${greeting} gift!` )
+  }
+  return cardsMessage;
+};
+
+
+function countDown(startingNumber) {
+    while (startingNumber > 0) {
+      console.log(startingNumber);
+      startingNumber--;
+    }
+    console.log(startingNumber);
+};
