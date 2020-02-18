cae025f92b71f5cb67427d36f694ece1dca89098
Niko Dixon <nikodixon@gmail.com>
2020-02-18 12:29:31 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(array, string) 
+ { let array1 = []
+    for (let i = 0; i < array.length; i++) {
+        array1.push(`Thank you, ${array[i]}, for the wonderful ${string} gift!`)
+    }
+    return array1
+}
+
+
+
+function countDown(num) {
+   while (num >= 0) {
+   console.log(num--)
+    }
+}
