3e92f9e887168ea6edc04f1315b38e6a47b6a111
Gonzalez32 <3232rose@gmail.com>
2020-02-17 22:59:16 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+    let cardArray = []
+    for (let i = 0; i < names.length; i++) {
+        cardArray.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+    return cardArray
+}
+
+function countDown(startingNumber) {
+    while (startingNumber > -1) {
+        console.log(startingNumber); 
+        startingNumber -= 1; 
+    }
+} 
